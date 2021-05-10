43d17ab15444f45a0006f8050b3d294528e0c363
Ahmad Alhashemi <ahmad@ahmadh.com>
2021-05-11 01:20:39 +0300

    Initial Commit

---
+++ Shared/ConwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConwayApp.swift
+//  Shared
+//
+//  Created by Ahmad Alhashemi on 2021-05-11.
+//
+
+import SwiftUI
+
+@main
+struct ConwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
