4e05d316d15253fb595bc12c2190879e062430dc
Ahmad Alhashemi <ahmad@ahmadh.com>
2021-05-11 23:17:39 +0300

    Add autoplay feature

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -33,15 +33,27 @@ struct ConwayCanvas: Shape {
 
 struct ContentView: View {
   @State var conway = Conway(size: 96)
+  @State var autoPlay = true
+  let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
 
   var body: some View {
-    ConwayCanvas(conway: conway)
-      .fill(Color.blue)
-      .aspectRatio(1, contentMode: .fit)
-      .background(Color.white)
-      .onTapGesture {
-        conway.nextGeneration()
+    VStack {
+      ConwayCanvas(conway: conway)
+        .fill(Color.blue)
+        .aspectRatio(1, contentMode: .fit)
+        .background(Color.white)
+        .onReceive(timer) { _ in
+          if autoPlay {
+            conway.nextGeneration()
+          }
+        }
+      
+      HStack {
+        Toggle("Autoplay", isOn: $autoPlay)
+        Button("Next") { conway.nextGeneration() }
       }
+      .padding(10)
+    }
   }
 }
 
