0a4dc7a8dc542405b2fb690ac7b012fd84e37f56
Ahmad Alhashemi <ahmad@ahmadh.com>
2021-06-09 20:07:12 +0300

    Factor cell level operations into a struct

--- Shared/Conway.swift
+++ Shared/Conway.swift
@@ -6,13 +6,32 @@
 //
 
 struct Conway {
-  private var grid: [UInt8]
+  private struct Cell {
+    private var data: UInt8
+    
+    init() { self.data = 0 }
+
+    var alive: Bool {
+      get { data & 1 == 1 }
+      set { if newValue { data |= 1} else { data &= ~1 } }
+    }
+    
+    var isBlank: Bool { data == 0 }
+
+    var neighbours: UInt8 { data >> 1 }
+    mutating func increment() { data += 0b10 }
+    mutating func decrement() { data -= 0b10 }
+    
+    mutating func clear() { data = 0 }
+  }
+  
+  private var grid: [Cell]
   let size: Int
   
   subscript(row: Int, col: Int) -> Bool {
-    get { grid[row * size + col] & 1 == 1 }
+    get { grid[row * size + col].alive }
     set {
-      func updateNeighbours(row: Int, col: Int, action: (inout UInt8) -> ()) {
+      func updateNeighbours(row: Int, col: Int, action: (inout Cell) -> ()) {
         func wrap(_ i: Int) -> Int { (i + size) % size }
         
         action(&grid[wrap(row - 1) * size + wrap(col - 1)])
@@ -30,11 +49,11 @@ struct Conway {
       guard newValue != self[row, col] else { return }
       
       if newValue {
-        grid[row * size + col] |= 1
-        updateNeighbours(row: row, col: col) { $0 += 0b10 }
+        grid[row * size + col].alive = true
+        updateNeighbours(row: row, col: col) { $0.increment() }
       } else {
-        grid[row * size + col] &= ~1
-        updateNeighbours(row: row, col: col) { $0 -= 0b10 }
+        grid[row * size + col].alive = false
+        updateNeighbours(row: row, col: col) { $0.decrement() }
       }
     }
   }
@@ -45,9 +64,9 @@ struct Conway {
     for r in 0..<size {
       for c in 0..<size {
         let cell = original[r * size + c]
-        guard cell != 0 else { continue }
+        guard !cell.isBlank else { continue }
         
-        switch (cell >> 1) {
+        switch cell.neighbours {
         case 3: self[r, c] = true  // lives
         case 2: break              // doesn't change
         case _: self[r, c] = false // dies
@@ -57,7 +76,7 @@ struct Conway {
   }
   
   mutating func clear() {
-    self.grid.resetBytes(in: 0..<(size*size))
+    for i in grid.indices { grid[i].clear() }
   }
   
   mutating func randomize() {
@@ -72,6 +91,6 @@ struct Conway {
   
   init(size: Int) {
     self.size = size
-    self.grid = Array(repeating: 0, count: size * size)
+    self.grid = Array(repeating: .init(), count: size * size)
   }
 }
